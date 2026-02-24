be0dfde8d8edb040d3764ebb25726e96ca632328
Emmanuel Amador <emmanuel.amador@endava.com>
2026-02-24 02:08:01 +0000

    Add APIRegistry tool to the agent

--- agent/core_agent/agent.py
+++ agent/core_agent/agent.py
@@ -4,6 +4,7 @@ import vertexai
 from vertexai import agent_engines
 from google.genai.types import GenerateContentConfig, ModelArmorConfig
 from google.adk.agents import Agent
+from google.adk.tools.api_registry import ApiRegistry
 from .config import GCPConfig, AgentConfig
 
 gcp_config = GCPConfig()
@@ -32,11 +33,32 @@ agent_settings = GenerateContentConfig(
     # ),
 )
 
+# Configure with your Google Cloud Project ID and registered MCP server name
+MCP_SERVER_NAME = f"projects/{project_id}/locations/global/mcpServers/google-discoveryengine.googleapis.com-mcp"
+
+# Example header provider for BigQuery, a project header is required.
+def header_provider(context):
+    return {"x-goog-user-project": project_id}
+
+# Initialize ApiRegistry
+api_registry = ApiRegistry(
+    api_registry_project_id=project_id,
+    header_provider=header_provider
+)
+
+# Get the toolset for the specific MCP server
+registry_tools = api_registry.get_toolset(
+    mcp_server_name=MCP_SERVER_NAME,
+    # Optionally filter tools:
+    #tool_filter=["list_datasets", "run_query"]
+)
+
 root_agent = Agent(
     model=agent_config.MODEL_NAME,
     name="research_agent",
     generate_content_config=agent_settings,
     instruction="You are a helpful research assistant.",
+    tools=[registry_tools]
 )
 
 
